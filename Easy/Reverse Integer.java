cb724cda651b03e6c1b9311a9bff869e69d3a508
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2020-12-23 15:46:54 +0200

    Update Reverse Integer.java

--- Easy/Reverse Integer.java
+++ Easy/Reverse Integer.java
@@ -7,15 +7,15 @@ public class ReverseInteger
 		
 		while (x != 0)
 		{
-            modulu = x % 10;
-            
-            if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE / 10 && modulu >= 8)
-                return 0;
-            if (result < Integer.MIN_VALUE / 10 || result == Integer.MIN_VALUE / 10 && modulu < -9)
-                return 0;
-            
-            result = (result * 10) + modulu;
-			x /= 10;
+		    modulu = x % 10;
+
+		    if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE / 10 && modulu >= 8)
+			return 0;
+		    if (result < Integer.MIN_VALUE / 10 || result == Integer.MIN_VALUE / 10 && modulu < -9)
+			return 0;
+
+		    result = (result * 10) + modulu;
+		    x /= 10;
 		}
 		return result;
 	}
