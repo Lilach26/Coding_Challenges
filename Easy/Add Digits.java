d7992f4fea1472ab4e7fd99061f604a5cc3bfe85
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2020-12-29 00:52:30 +0200

    Update Add Digits.java

--- Easy/Add Digits.java
+++ Easy/Add Digits.java
@@ -13,17 +13,17 @@ class Solution
     {
         int result = 0;
 		
-		while (num > 0)
-		{
-			result += num % 10;
-			num /= 10;
+	while (num > 0)
+	{
+		result += num % 10;
+		num /= 10;
 			
-			if (num == 0 && result > 9)
-			{
-				num = result;
-				result = 0;
-			}
+		if (num == 0 && result > 9)
+		{
+			num = result;
+			result = 0;
 		}
-		return result;    
+	}
+	return result;    
     }
 }
