a301500a7a44b0a4a501b61b5f1a979f9c36b664
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2021-01-06 23:41:19 +0200

    Create Monotonic Array.java

---
+++ Easy/Monotonic Array.java
@@ -0,0 +1,58 @@
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+Return true if and only if the given array A is monotonic.
+
+Example 1:
+Input: [1,2,2,3]
+Output: true
+
+Example 2:
+Input: [6,5,4,4]
+Output: true
+
+Example 3:
+Input: [1,3,2]
+Output: false
+
+Example 4:
+Input: [1,2,4,5]
+Output: true
+
+Example 5:
+Input: [1,1,1]
+Output: true
+*/
+
+
+public class MonotonicArray 
+{
+	public boolean isMonotonic(int[] A)
+	{
+		boolean increase = false, decrease = false;
+		int i, size = A.length;
+		
+		for (i = 0; i < size - 1; i++)
+		{
+			if (A[i] < A[i + 1])
+			{
+				increase = true;
+			}
+			else if (A[i] > A[i + 1])
+			{
+				decrease = true;
+			}
+			
+			if (increase && decrease)
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		MonotonicArray sol = new MonotonicArray();
+		int[] arr = new int[] {1,3,2};
+		System.out.print(sol.isMonotonic(arr));
+	}
+}
