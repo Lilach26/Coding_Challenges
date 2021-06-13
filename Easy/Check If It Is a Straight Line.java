847167b9e63e61f8645773e115f8ba639fc893ab
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2021-06-14 00:06:45 +0300

    Create Check If It Is a Straight Line.java

---
+++ Easy/Check If It Is a Straight Line.java
@@ -0,0 +1,42 @@
+/*
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+Example 1:
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+
+Example 2:
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+Constraints:
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+*/
+
+class Solution 
+{
+    public boolean checkStraightLine(int[][] coordinates) 
+    {
+        int n = coordinates.length, temp;
+        
+        if (n == 2)
+        {
+            return true;
+        }
+        
+        for (int i = 0; i < n - 2; i++)
+        {
+            temp = ((coordinates[i+1][1]-coordinates[i][1])*(coordinates[i+2][0]-coordinates[i][0]))-((coordinates[i+2][1]-coordinates[i][1])*(coordinates[i+1][0]-coordinates[i][0]));
+            
+            if (temp != 0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
